35883fc146f961c69dba2f8263be545599470c84
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2022-07-06 21:32:03 +0900

    readonly

--- tools/deploy_and_benchmark.sh
+++ tools/deploy_and_benchmark.sh
@@ -1,30 +1,30 @@
-REPO_ROOT_DIR=/home/isucon/private_isu
-NGINX_ACCESS_LOG=/var/log/nginx/access.log
-NGINX_ERROR_LOG=/var/log/nginx/error.log
-MYSQL_SLOW_LOG=/var/log/mysql/mysql-slow.log
-RESULT_BASE_DIR=$REPO_ROOT_DIR/result
-BENCHMARKER_INSTANCE_PRIVATE_IP=172.31.46.66
-APP_INSTANCE_PRIVATE_IP=172.31.43.134
-MYSQL_DEPLOY_DIR=$REPO_ROOT_DIR/sql
-MYSQL_CONF_DIR=/etc/mysql/mysql.conf.d
-NGINX_CONF_DIR=/etc/nginx
+readonly REPO_ROOT_DIR=/home/isucon/private_isu
+readonly NGINX_ACCESS_LOG=/var/log/nginx/access.log
+readonly NGINX_ERROR_LOG=/var/log/nginx/error.log
+readonly MYSQL_SLOW_LOG=/var/log/mysql/mysql-slow.log
+readonly RESULT_BASE_DIR=$REPO_ROOT_DIR/result
+readonly BENCHMARKER_INSTANCE_PRIVATE_IP=172.31.46.66
+readonly APP_INSTANCE_PRIVATE_IP=172.31.43.134
+readonly MYSQL_DEPLOY_DIR=$REPO_ROOT_DIR/sql
+readonly MYSQL_CONF_DIR=/etc/mysql/mysql.conf.d
+readonly NGINX_CONF_DIR=/etc/nginx
 
 if (( $# != 1 )); then
   echo "Usage: $0 <change log>"
   exit 1
 else
-  changelog=$1
+  readonly changelog=$1
 fi
 
 set -ux
 
 # get latest changes
 git pull
-commit_id=$(git rev-parse HEAD)
+readonly commit_id=$(git rev-parse HEAD)
 
 # create result dir
-dt=$(date +%Y%m%d-%H%M%S)
-result_dir=${RESULT_BASE_DIR}/${dt}_${commit_id}
+readonly dt=$(date +%Y%m%d-%H%M%S)
+readonly result_dir=${RESULT_BASE_DIR}/${dt}_${commit_id}
 mkdir -p $result_dir
 
 ###
@@ -69,15 +69,15 @@ sudo systemctl restart isu-go
 ###
 
 # start collectl
-collectl_result_dir=$result_dir/collectl
+readonly collectl_result_dir=$result_dir/collectl
 mkdir -p $collectl_result_dir
 collectl -scdm -f $collectl_result_dir -P &
-collectl_job_id=$!
+readonly collectl_job_id=$!
 
 ###
 # run benchmark
 ###
-benchmark_result_dir=$result_dir/benchmark
+readonly benchmark_result_dir=$result_dir/benchmark
 mkdir -p $benchmark_result_dir
 cmd="/home/isucon/private_isu.git/benchmarker/bin/benchmarker -u /home/isucon/private_isu.git/benchmarker/userdata -t http://${APP_INSTANCE_PRIVATE_IP}"
 ssh -i ~/.ssh/for_benchmarker isucon@$BENCHMARKER_INSTANCE_PRIVATE_IP $cmd > $benchmark_result_dir/benchmarker.log
@@ -91,25 +91,25 @@ kill -SIGINT $collectl_job_id
 colplot -dir $collectl_result_dir -plots cpu,disk,mem -filetype png -filedir $collectl_result_dir -height 0.5 -lastmins 3
 
 # alp
-alp_result_dir=$result_dir/alp
+readonly alp_result_dir=$result_dir/alp
 mkdir -p $alp_result_dir
 sudo alp json --file $NGINX_ACCESS_LOG --sort=sum -r > $alp_result_dir/alp.log
 
 # copy nginx access & error log
-nginx_result_dir=$result_dir/nginx
+readonly nginx_result_dir=$result_dir/nginx
 mkdir -p $nginx_result_dir
 sudo gzip --best -c $NGINX_ACCESS_LOG > $nginx_result_dir/access.log.gz
 sudo gzip --best -c $NGINX_ERROR_LOG > $nginx_result_dir/error.log.gz
 
 # analyze mysql slow query log
-mysql_result_dir=$result_dir/mysql
+readonly mysql_result_dir=$result_dir/mysql
 mkdir -p $mysql_result_dir
 sudo mysqldumpslow $MYSQL_SLOW_LOG > $mysql_result_dir/mysqldumpslow.log
 sudo pt-query-digest $MYSQL_SLOW_LOG > $mysql_result_dir/pt-query-digest.log
-sudo gzip --best -c $MYSQL_SLOW_LOG > $mysql_result_dir/mysql-slow.log.gz
+# sudo gzip --best -c $MYSQL_SLOW_LOG > $mysql_result_dir/mysql-slow.log.gz  # 重すぎる
 
 # add summary row
-jqout=$(jq -r '[.pass,.score,.success,.fail] | @tsv' $benchmark_result_dir/benchmarker.log | tr '\t' '|')
+readonly jqout=$(jq -r '[.pass,.score,.success,.fail] | @tsv' $benchmark_result_dir/benchmarker.log | tr '\t' '|')
 echo "|${dt}|${jqout}|${commit_id}|${changelog}|" >> $RESULT_BASE_DIR/summary.md
 
 # git push
