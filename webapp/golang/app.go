6ce0ce3e8d31a5dc30b628c37fb0762c3b9ef48e
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2022-07-08 01:05:33 +0900

    make global!!!

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -111,7 +111,7 @@ func init() {
 	if memdAddr == "" {
 		memdAddr = "localhost:11211"
 	}
-	memcacheClient := memcache.New(memdAddr)
+	memcacheClient = memcache.New(memdAddr)
 	store = gsm.NewMemcacheStore(memcacheClient, "iscogram_", []byte("sendagaya"))
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
